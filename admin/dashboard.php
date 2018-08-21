633990fab9cb112c9b77d31b2264c53f510b1e1f
Techbinod <35165977+Techbinod@users.noreply.github.com>
2018-08-21 09:06:59 +0545

    video files createad for video management

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -24,8 +24,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
+                           Cardinal International Boarding School
                         </h1>
                         <ol class="breadcrumb">
                             <li>
